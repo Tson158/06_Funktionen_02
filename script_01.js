d44b2531300464ae285ec2960792630a5b65ccc9
Kakarott <Sonntag_thomas_86@web.de>
2021-11-23 09:07:09 +0100

    Addition a+b

--- script_01.js
+++ script_01.js
@@ -1,16 +1,27 @@
 
 /*** Rechner "App" */
-/*
-0. a+b / a-b/ a*b / a/b  // ergebnis c
-1. Dateneingabe + -überprüfung 
-2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
-*/
+
+// 0. a+b / a-b/ a*b / a/b  // ergebnis c
+// 1. Dateneingabe + -überprüfung 
+// 2. Auswahl Rechenart : 
+// 3. Fkt. Grundrechenarten : 
+
+// 3.1 Modul: Addition a+b | Test
+
+ausgabe(addieren(2,1));
+function addieren(a,b) {
+    let c = a + b;
+    return c;
+}
+
+// Modul: Addition a+b | Test
+
+
+// 4. Ausgabe in Konsole : check!
 
 // Modul: Konsolenausgabe | Test:
 
-ausgabe("Hallo Penner!")
+// ausgabe("Hallo Penner!")
 function ausgabe(outputData) {
     console.log(outputData);
 }
