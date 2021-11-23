d17751d802b0d0b896fdce4e72cdfa32370ebcd3
Kakarott <Sonntag_thomas_86@web.de>
2021-11-23 12:20:30 +0100

    Rechenart ++

--- script_01.js
+++ script_01.js
@@ -5,33 +5,65 @@
 // 1. Dateneingabe + -überprüfung 
 
 // 2. Auswahl Rechenart : 
+//  Vereinbarung : "+", "-", "*", "/"
+ausgabe(rechner(29,204,"+"))
+ausgabe(rechner(26,12,"-"))
+ausgabe(rechner(2,32,"*"))
+ausgabe(rechner(20,2,"/"))
+ausgabe(rechner(2,7,"#"))
+ausgabe(rechner(2,0,"/"))
+function rechner(a,b,op) {
+    switch (op) {
+        case "+": // Addition
+            return addieren(a,b);
+        
+         case "-": // Subtraktion
+            return Subtraktion(a,b);
+            
+         case "*": // Multiplikation
+            return Multiplikation(a,b);
+            
+         case "/": // Division
+            return Division(a,b);
+
+        default: // wenn was nicht stimmt (falches zeichen oder buchstaben)
+            return "Fehler sitzt vorm Monitor!"
+    }
+}
+
+
+
+
+
+
+/************************************************************/
 
 // 3. Fkt. Grundrechenarten : 
 
 // 3.1 Modul: Addition a+b | Test
 
-ausgabe(addieren(7,3));
+// ausgabe(addieren(7,3));
 function addieren(a,b) {
     return a + b;
 }
 
 // 3.2 Modul: Subtraktion a-b | Test
 
-ausgabe(Subtraktion(6,7));
+// ausgabe(Subtraktion(6,7));
 function Subtraktion(a,b) {
     return a - b;
 }
 
 // 3.3 Modul: Multiplikation a*b | Test
 
-ausgabe(Multiplikation(5,3));
+// ausgabe(Multiplikation(5,3));
 function Multiplikation(a,b) {
     return a * b;
 }
 
 // 3.4 Modul: Division a/b | Test
 
-ausgabe(Division(10,0));
+// ausgabe(Division(10,0));
 function Division(a,b) 
 {
     if (b != 0) 
